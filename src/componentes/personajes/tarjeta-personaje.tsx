955930f695e7ad67acac8d5d571ede817d100667
andreortegate <acevedo9611@gmail.com>
2023-11-06 11:33:15 -0500

    favorito

--- src/componentes/personajes/tarjeta-personaje.tsx
+++ src/componentes/personajes/tarjeta-personaje.tsx
@@ -8,20 +8,17 @@ interface TarjetaPersonajeProps {
   esFavorito: boolean;
 }
 
-/**
- * Tarjeta para cada personaje dentro de la grilla de personajes. 
- * Deberás agregar las propiedades necesarias para mostrar los datos de los personajes
- * 
- * @returns un JSX element 
- */
 const TarjetaPersonaje: React.FC<TarjetaPersonajeProps> = ({ nombre, imagenSrc, esFavorito }) => {
+  const handleFavoritoClick = () => {
+    // Tu lógica de manejo de clic aquí
+  };
+
   return (
     <div className="tarjeta-personaje">
       <img src={imagenSrc} alt={nombre} />
       <div className="tarjeta-personaje-body">
         <span>{nombre}</span>
-        <BotonFavorito esFavorito={false} onClick={() => { /* Tu lógica de manejo de clic aquí */ }} />
-
+        <BotonFavorito esFavorito={esFavorito} onClick={handleFavoritoClick} />
       </div>
     </div>
   );
